fb90b331f972cd7ceda1a0f8ec03c87a7db85592
Ping2win <40270221+PINGWINbl4@users.noreply.github.com>
2021-09-24 20:12:31 +0500

    Введена проверка строки на корректность

--- perebor.py
+++ perebor.py
@@ -1,10 +1,20 @@
 alpabet = ["a", 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
            's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-x = str(input())
-print('rot = ')
-rot = int(input())
+x = str(input('Введите сообщение на английском языке'))
 x = x.lower()
 x = list(x)
+p = True
+while p:
+    for i in range(len(x)):
+        if x[i] in alpabet:
+            p = False
+        else:
+            x = str(input('Сообщение некорректно, введите новое'))
+            x = x.lower()
+            x = list(x)
+            p = True
+print('rot = ')
+rot = int(input())
 if " " in x:
     x.remove(' ')
 for i in range(len(x)):
@@ -14,4 +24,4 @@ for i in range(len(x)):
         if d == 25:
             d = 0
     x[i] = alpabet[(d+rot) % 26]
-print(x)
+print('Ваша закодированная строка',*x)
