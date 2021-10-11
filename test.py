85ef344d68534d47dfa91768aa8da5162587b3b9
Ping2win <40270221+PINGWINbl4@users.noreply.github.com>
2021-10-11 12:34:29 +0500

    Добавлены комментарии
    Обработка переменных и диалог с пользователем были добавлены ранее

--- test.py
+++ test.py
@@ -1,27 +1,27 @@
 alphabet = []
-for i in range(97, 123):
+for i in range(97, 123):    # Формирование алфавита
     alphabet.append(chr(i))
 alphabet.append(' ')
 
-string = str(input('Сообщение на английском'))
+string = str(input('Сообщение на английском')) # Обработка строки в нужном формате
 string.lower()
 dlina = len(string)
 
-for i in range(dlina):
+for i in range(dlina):  # Проверка строки на нахождение в алфавите
     if string[i] not in alphabet:
         string = str(input('Строка не корректна'))
         string.lower()
         i = 0
         dlina = len(string)
 
-while True:
+while True:     # Ввод сдвига и проверка его на ошибки ввода
     try:
         rot = int(input("введите сдвиг"))
         break
     except(ValueError):
         print('Некорректное значение')
 
-for i in range(len(string)):
+for i in range(len(string)): #Реализация сдвига эллемента строки в соответствии с алфавитом
     if string[i] != ' ':
         char = (ord(string[i])+rot)
         if char > 122:
